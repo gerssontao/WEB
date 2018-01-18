e7b57474d6fecb41cc46738a68678d3e1a954819
Unknown <gersson.tao@gmail.com>
2018-01-18 09:45:58 -0500

    Inicial

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2017, 12, 24, 16)}); // year, month, date, hour
+        });
+});		
+
